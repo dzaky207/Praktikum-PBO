2b812c767fa0038126aaab2870ff90db09fd3543
zett207 <36437054+dzaky207@users.noreply.github.com>
2024-11-15 18:12:42 +0700

    Create Pertemuan_2.java

---
+++ Pertemuan-2/Guided/Pertemuan_2.java
@@ -0,0 +1,17 @@
+public class Pertemuan_2 {
+
+    public static void main(String[] args) {
+        
+        int umur = 21;
+        float beratBadan = 65.5f;
+        char jenisKelamin = 'L';
+        boolean mahasiswa = true;
+        
+        //menampilkan nilai variable
+        
+        System.out.println("umur: " + umur);
+        System.out.println("Berat Badan: " + beratBadan + "Kg");
+        System.out.println("Jenis Kelamin: " + jenisKelamin);
+        System.out.println("Mahasiswa: " + mahasiswa);
+    }
+}
