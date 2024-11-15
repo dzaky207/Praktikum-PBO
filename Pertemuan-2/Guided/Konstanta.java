22596cb3a100a8d23a28ba72c1625f4177a474da
zett207 <36437054+dzaky207@users.noreply.github.com>
2024-11-15 18:10:40 +0700

    Create Konstanta.java

---
+++ Pertemuan-2/Guided/Konstanta.java
@@ -0,0 +1,10 @@
+public class Konstanta {
+    public static void main(String[]args) {
+        //konstanta
+        final double PI = 3.14159;
+        int radius = 5; 
+        double luasLingkaran = PI * radius * radius;
+        
+        System.out.println("Luas lingkaran dengan radius: " + radius + " adalah " + luasLingkaran);
+    }
+}
