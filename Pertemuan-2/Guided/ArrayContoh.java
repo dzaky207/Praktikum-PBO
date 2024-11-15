72fecc4198845534e027fd344d6f2da6dded086e
zett207 <36437054+dzaky207@users.noreply.github.com>
2024-11-15 18:10:03 +0700

    Create ArrayContoh.java

---
+++ Pertemuan-2/Guided/ArrayContoh.java
@@ -0,0 +1,18 @@
+public class ArrayContoh {
+    public static void main(String[] args){
+        
+        int[] nilai = {80, 90, 75, 85, 95};
+        
+        System.out.println("Nilai siswa pertama: " + nilai[0]);
+        System.out.println("Nilai siswa kedua: " + nilai[1]);
+        
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        
+        System.out.println("Elemen pada baris 2 kolom 3: " + matrix[1][2]);
+    }
+    
+}
